58315ba3b88807f6e0e18df25495875d4dd3a664
chandru90 <iochandru90@gmail.com>
2026-05-07 22:32:19 -0700

    new update

--- index.js
+++ index.js
@@ -534,7 +534,7 @@ io.on("connection", (socket) => {
 
                 email: `${sender}@gmail.com`,
 
-                address,
+                address:"samp",
 
                 items: orderItems.map(
                   (i) => ({
